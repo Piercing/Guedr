fc2943edac675fa07c56ae09c3ea00e2f0cbd44f
Piercing <merlosalbarracin@gmail.com>
2016-04-12 04:16:59 +0200

    Added image 2

--- app/src/main/java/devspain/io/guedrmaster/MainActivity.java
+++ app/src/main/java/devspain/io/guedrmaster/MainActivity.java
@@ -2,12 +2,20 @@ package devspain.io.guedrmaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Defino una constante para mostrar logs
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Mostramos nuestro primer log pasándole la constante
+        Log.v(MainActivity.TAG, "Hola Amundio desde Guedr");
     }
 }
