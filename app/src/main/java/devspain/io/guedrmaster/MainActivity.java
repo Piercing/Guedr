877a2a121d4e8f16266c7c0a8c22c2f1b9fb4cb0
Piercing <merlosalbarracin@gmail.com>
2016-04-13 03:39:44 +0200

    Finished  tree methods for to call onClick

--- app/src/main/java/devspain/io/guedrmaster/MainActivity.java
+++ app/src/main/java/devspain/io/guedrmaster/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Defino una constante para mostrar logs
     private static final String TAG = "MainActivity";
@@ -28,25 +28,45 @@ public class MainActivity extends AppCompatActivity {
 
         // Asociamos controlador con vista a través del indentificador
         Button change2AmericanSystem = (Button) findViewById(R.id.change_to_american_btn);
-        // Le decimos que pasas cuando se pulsa el botón
-        // Para eso le pasamos una instancia de  alguién
-        // que implemetna la interfaz OnClickActionListener
+        Button change2EuropeanSystem = (Button) findViewById(R.id.change_to_european_btn);
 
-        change2AmericanSystem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Al ser pulsado el botón se dispara este método de esta clase anónima
-                changeToAmericanSystem(v);
-            }
-        });
+        // Le decimos que pasa cuando se pulsa el botón, le paso
+        // la propia vista que ya implemta --->  onClickListener.
+        // Para eso le paso una instancia de alguien que implementa la inrefaz OnClisListener
+        change2AmericanSystem.setOnClickListener(this);
+
+        // Hacemos lo propio con el botón 'europeo'
+        change2EuropeanSystem.setOnClickListener(this);
     }
 
     public void changeToAmericanSystem(View view) {
         Log.v(MainActivity.TAG, "Se llamó a change2AmericanSystem");
         // Cambiamos la imagen a mostrar
         // Le decimos al 'id' de la ImageView -> 'forecastImage' cual va a ser su imagen
+        forecastImage.setImageResource(R.drawable.offline_weather);
+    }
+
+    public void changeToEuropeanSystem(View view) {
+        Log.v(MainActivity.TAG, "Se llamó a change2EuropeanSystem");
+        // Cambiamos la imagen a mostrar
+        // Le decimos al 'id' de la ImageView -> 'forecastImage' cual va a ser su imagen
         forecastImage.setImageResource(R.drawable.offline_weather2);
     }
 
+    @Override
+    public void onClick(View v) {
+
+        // Compruebo el id del botón que llama
+        switch (v.getId()) {
+            // Se ha pulsado el botón americano
+            case R.id.change_to_american_btn:
+                changeToAmericanSystem(v);
+                break;
+            case R.id.change_to_european_btn:
+                // Se ha pulsado el europeo
+                changeToEuropeanSystem(v);
+                break;
+        }
+    }
 }
 
